e546784ca6812bc99a1fa857fa1d7fd3291a0168
Jacob Smith <jacobsmithsinbox@gmail.com>
2026-03-08 00:26:57 -0700

    Closes #213 (#247)

--- src/data/double_queue/double_queue.h
+++ src/data/double_queue/double_queue.h
@@ -1,7 +1,7 @@
 /** !
- * Include header for double queue library
+ * double ended queue interface
  * 
- * @file double_queue/double_queue.h 
+ * @file src/data/double_queue/double_queue.h 
  * 
  * @author Jacob Smith
  */
@@ -27,116 +27,183 @@
 struct double_queue_s;
 
 // type definitions
-/** !
- *  @brief The type definition of a double queue struct
- */
 typedef struct double_queue_s double_queue;
 
-// allocaters
+// function declarations
+/// constructors
 /** !
- *  Allocate memory for a double queue
+ * Construct a double queue with a specific number of entries
  *
- * @param pp_double_queue return
- *
- * @return 1 on success, 0 on error
- */
-int double_queue_create ( double_queue **const pp_double_queue );
-
-// constructors
-/** !
- *  Construct a double queue with a specific number of entries
- *
- * @param pp_double_queue return
+ * @param pp_double_queue result
  *
  * @return 1 on success, 0 on error
  */
 int double_queue_construct ( double_queue **const pp_double_queue );
 
 /** !
- *  Construct a double queue from a void pointer array
+ * Construct a double ended queue from an array 
  *
- * @param pp_double_queue return
- * @param pp_contents     pointer to array of void pointers to use as double queue contents.
- * @param size            number of double queue entries. 
+ * @param pp_double_queue result
+ * @param pp_contents     pointer to array
+ * @param size            quantity of double queue entries 
  *
  * @return 1 on success, 0 on error
  */
 int double_queue_from_contents ( double_queue **const pp_double_queue, void * const* const pp_contents, size_t size );
 
-// accessors
+/// accessors
 /** !
- *  Get the element at the front of the double queue
+ *  Get the element at the front of a double ended queue
  *
- * @param p_double_queue double queue
+ * @param p_double_queue the double ended queue
  *
- * @return element value on success, null pointer on error
+ * @return element on success, NULL on error
  */
 int double_queue_front ( double_queue *const p_double_queue, void **const pp_value );
 
 /** !
- *  Get the element at the rear of the double queue
+ *  Get the element at the rear of a double ended queue
  *
- * @param p_double_queue double queue
+ * @param p_double_queue the double ended queue
  *
- * @return element on success, null pointer on error
+ * @return element on success, NULL on error
  */
 int double_queue_rear ( double_queue *const p_double_queue, void **const pp_value );
 
 /** !
- *  Check if a double queue is empty
- *
- * @param p_double_queue double queue
- *
- * @return true if double queue is empty else false
+ * Get the quantity of elements a double ended queue
+ * 
+ * @param p_double_queue the double ended queue 
+ * 
+ * @return the quantity of the elements in the double ended queue
  */
-bool double_queue_empty ( const double_queue *const p_double_queue );
+size_t double_queue_size ( double_queue *const p_double_queue );
 
-// mutators
 /** !
- *  Add an element to the front of a double queue
+ * Test if a double ended queue is empty
+ *
+ * @param p_double_queue the double ended queue
+ *
+ * @return true IF double ended queue is empty ELSE false
+ */
+bool double_queue_empty ( double_queue *const p_double_queue );
+
+/// mutators
+/** !
+ * Add an element to the front of a double ended queue
  *
  * @param p_double_queue the double queue
- * @param data           element to add to the double queue
- *
+ * @param data           the element
+ * 
  * @return 1 on success, 0 on error
  */
-int double_queue_front_add ( double_queue *const p_double_queue, void *const data );
+int double_queue_front_add ( double_queue *const p_double_queue, void *const p_value );
 
 /** !
- *  Remove an element from the front of a double queue
+ * Remove an element from the front of a double ended queue
  *
  * @param p_double_queue the double queue
- * @param pp_value       return
+ * @param pp_data        result IF not null ELSE ignored
  *
  * @return 1 on success, 0 on error
  */
 int double_queue_front_remove ( double_queue *const p_double_queue, void **const pp_value );
 
 /** !
- *  Add an element to the rear of a double queue
+ * Add an element to the rear of a double ended queue
  *
  * @param p_double_queue the double queue
- * @param data           element to add to the double queue
+ * @param data           the element
  *
  * @return 1 on success, 0 on error
  */
-int double_queue_rear_add ( double_queue *const p_double_queue, void *const data );
+int double_queue_rear_add ( double_queue *const p_double_queue, void *const p_value );
 
 /** !
- *  Remove an element from the rear of a double queue
+ * Remove an element from the rear of a double ended queue
  *
- * @param p_double_queue the double queue
- * @param pp_value       return
+ * @param p_double_queue the double ended queue
+ * @param pp_data        result IF not null ELSE ignored
  *
  * @return 1 on success, 0 on error
  */
 int double_queue_rear_remove ( double_queue *const p_double_queue, void **const pp_value );
 
+/// map
 /** !
- * Destroy a double queue
+ * Apply an operation to each element in a double ended queue,
+ * optionally releasing elements
+ *
+ * @param p_double_queue the double ended queue
+ * @param pfn_map        pointer to map function
+ * @param pfn_allocator  pointer to allocator function
  * 
- * @param pp_double_queue pointer to double queue pointer
+ * @return 1 on success, 0 on error
+ */
+int double_queue_map ( double_queue *const p_double_queue, fn_map *pfn_map, fn_allocator *pfn_allocator );
+
+/// iterators
+/** !
+ * Call function on every element in a double ended queue
+ *
+ * @param p_double_queue the double ended queue
+ * @param pfn_foreach    pointer to foreach function
+ * 
+ * @return 1 on success, 0 on error
+ */
+int double_queue_foreach ( double_queue *const p_double_queue, fn_foreach *pfn_foreach );
+
+/** !
+ * Call function on every element in a double ended queue
+ *
+ * @param p_double_queue the double ended queue
+ * @param pfn_fori       pointer to fori function
+ * 
+ * @return 1 on success, 0 on error
+ */
+int double_queue_fori ( double_queue *const p_double_queue, fn_fori *pfn_fori );
+
+/// reflection
+/** !
+ * Pack a double ended queue into a buffer
+ * 
+ * @param p_buffer       result
+ * @param p_double_queue the double ended queue
+ * @param pfn_elemenet   pointer to pack function IF not null ELSE default
+ * 
+ * @return 1 on success, 0 on error
+ */
+int double_queue_pack ( void *p_buffer, double_queue *p_double_queue, fn_pack *pfn_element );
+
+/** !
+ * Unpack a buffer into a double ended queue
+ * 
+ * @param pp_double_queue result
+ * @param p_buffer        the buffer
+ * @param pfn_elemenet    pointer to unpack function IF not null ELSE default
+ * 
+ * @return 1 on success, 0 on error
+ */
+int double_queue_unpack ( double_queue **pp_double_queue, void *p_buffer, fn_unpack *pfn_element );
+
+/// hash
+/** !
+ * Compute a 64-bit hash of a double ended queue
+ * 
+ * @param p_double_queue the double ended queue
+ * @param pfn_element    hashing function applied to each element
+ * 
+ * @return hash on success, NULL on error
+ */
+hash64 double_queue_hash ( double_queue *p_double_queue, fn_hash64 *pfn_element );
+
+/// destructors
+/** !
+ * Destroy and deallocate a double ended queue
+ * 
+ * @param pp_double_queue pointer to double ended queue pointer
+ * @param pfn_allocator   pointer to allocator function for deallocating elements
  * 
  * @return 1 on success, 0 on error 
  */
-int double_queue_destroy ( double_queue **const pp_double_queue );
+int double_queue_destroy ( double_queue **const pp_double_queue, fn_allocator *pfn_allocator );
--- src/examples/double_queue_example.c
+++ src/examples/double_queue_example.c
@@ -1,108 +1,393 @@
 /** !
- * Double queue example
+ * Double ended queue example program
  * 
- * @file main.c
+ * @file src/examples/double_queue_example.c
  * 
  * @author Jacob Smith
  */
 
 // standard library
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 // gsdk
 /// core
 #include <core/log.h>
+#include <core/sync.h>
+#include <core/pack.h>
+#include <core/hash.h>
+#include <core/interfaces.h>
 
 /// data
 #include <data/double_queue.h>
 
+// enumeration definitions
+enum shapes_e
+{
+    CIRCLE         = 0,
+    SQUARE         = 1,
+    TRIANGLE       = 2,
+    RECTANGLE      = 3,
+    PENTAGON       = 4,
+    HEXAGON        = 5,
+	SHAPE_QUANTITY = 6
+};
+
+// forward declarations
+/// logs
+int checkpoint ( double_queue *p_double_queue, const char *p_event );
+
+// string
+fn_comparator string_compare;
+fn_fori string_print;
+fn_hash64 string_hash;
+fn_map string_upper_case;
+fn_map string_lower_case;
+fn_pack string_pack;
+fn_unpack string_unpack;
+
+// data
+/// immutable piece strings
+const char *_p_shapes[SHAPE_QUANTITY] =
+{
+    [CIRCLE]    = "Circle",
+	[SQUARE]    = "Square",
+	[TRIANGLE]  = "Triangle",
+	[RECTANGLE] = "Rectangle",
+	[PENTAGON]  = "Pentagon",
+	[HEXAGON]   = "Hexagon",
+};
+
+/// file for reflection
+FILE *p_f = NULL;
+size_t file_len = 0;
+
+/// hashes
+hash64 h1 = 0,
+       h2 = 0;
+
+/// working cache
+double_queue *p_double_queue = NULL;
+
 // entry point
-int main ( int argc, const char *argv[] )
+int main ( int argc, const char* argv[] )
 {
 
-	// unused
-	(void) argc;
-	(void) argv;
+    // unused
+    (void) argc;
+    (void) argv;
 
-	// initialized data
-	double_queue *p_double_queue = 0;
-	void         *value          = 0;
-	char         *pp_contents[]  = { "First!", "Second!", "Third!", "Fourth!", "Fifth!", "Sixth!", 0x0 };
+    // #0 - start
+    checkpoint(p_double_queue, "start");
+    
+    // #1 - initial
+    {
+        
+        // construct the cache
+        double_queue_construct(&p_double_queue);
+        
+        // checkpoint
+        checkpoint(p_double_queue, "after construction");
+    }
+        
+    // #2 - insert 
+    {
+        
+        // push some shapes
+        for (enum shapes_e _shape = CIRCLE; _shape < PENTAGON; _shape++)
+            double_queue_rear_add(p_double_queue, _p_shapes[_shape]);
+        
+        // checkpoint
+        checkpoint(p_double_queue,"after adding < Circle, Square, Triangle, Rectangle >");
+    }
+        
+    // #3 - peek front
+    {
+        
+        // initialized data
+        void *p_value = NULL;
+        
+        // peek the last element
+        double_queue_front(p_double_queue, &p_value);
+        
+        // print the top of the cache
+        printf("peek() -> %s\n", p_value);
 
-	// Construct a queue
-	double_queue_construct(&p_double_queue);
-	
-	// Add three elements to the queue
-	double_queue_rear_add(p_double_queue, "Third!");
-	double_queue_rear_add(p_double_queue, "Fourth!");
-	double_queue_front_add(p_double_queue, "Second!");
-	double_queue_front_add(p_double_queue, "First!");
+        // checkpoint
+        checkpoint(p_double_queue, "after peek front");
+    }
 
-	// Empty the queue and print each element
-	while ( double_queue_empty(p_double_queue) == false )
-	{
+    // #4 - peek rear
+    {
+        
+        // initialized data
+        void *p_value = NULL;
+        
+        // peek the last element
+        double_queue_rear(p_double_queue, &p_value);
+        
+        // print the top of the cache
+        printf("peek() -> %s\n", p_value);
 
-		// initialized data
-		void *v = 0;
+        // checkpoint
+        checkpoint(p_double_queue, "after peek rear");
+    }
 
-		// Dequeue an item
-		double_queue_front_remove(p_double_queue, &v);
+    // #5 - to binary
+    {
 
-		// Print the element
-		printf("%s\n", (char *) v);
-	}
-	
-	printf("\n");
+        // initialized data
+        char buf[1024] = { 0 };
+        
+        // open a file for writing
+        p_f = fopen("resources/reflection/double_queue.bin", "wb");
 
-	// Fill up the queue
-	double_queue_from_contents(&p_double_queue, (void **)pp_contents, 6);
+        // reflect the double ended queue to a buffer
+        file_len = double_queue_pack(buf, p_double_queue, string_pack),
+        
+        // write the buffer to a file
+        fwrite(buf, file_len, 1, p_f),
 
-	log_info(" - Ascending -\n");
+        // close the file
+        fclose(p_f),
 
-	// Empty the full queue
-	while ( double_queue_empty(p_double_queue) == false )
-	{
+        // checkpoint
+        checkpoint(p_double_queue, "after serialize");
+    }
+    
+    // #6 - hash 1
+    {
 
-		// initialized data
-		void *v = 0;
+        // initialized data
+        h1 = double_queue_hash(p_double_queue, (fn_hash64 *)string_hash);
 
-		// Dequeue an item
-		double_queue_front_remove(p_double_queue,&v);
+        // print the hash
+        printf("hash 1 -> 0x%llx\n", h1);
 
-		// Print the element
-		printf("%s\n", (char *) v);
-	}
+        // checkpoint
+        checkpoint(p_double_queue, "after hash 1");
+    }
 
-	// Destroy the double queue
-	// double_queue_destroy(&p_double_queue);
-	
-	// Fill up the queue
-	double_queue_from_contents(&p_double_queue, (void **)pp_contents, 6);
+    // #7 - remove front and rear
+    {
 
-	// Formatting
-	log_info("\n - Descending -\n");
+        // pop the first and last elements
+        double_queue_front_remove(p_double_queue, NULL),
+        double_queue_rear_remove(p_double_queue, NULL);
 
-	// Empty the full queue
-	while ( double_queue_empty(p_double_queue) == false )
-	{
+        // checkpoint
+        checkpoint(p_double_queue, "after removing < Circle, Rectangle >");
+    }
 
-		// initialized data
-		void *v = 0;
+    // #8 - add front and rear 
+    {
 
-		// Dequeue an item
-		double_queue_rear_remove(p_double_queue,&v);
+        // push some pieces
+        double_queue_front_add(p_double_queue, _p_shapes[PENTAGON]),
+        double_queue_rear_add(p_double_queue, _p_shapes[HEXAGON]);
 
-		// Print the element
-		printf("%s\n", (char *) v);
-	}
-	
-	// Cause an underflow
-	if ( double_queue_front_remove(p_double_queue, &value) == 0 )
-		log_warning("\nDetected double queue underflow!\n");
+        // checkpoint
+        checkpoint(p_double_queue, "after adding < Pentagon, Hexagon >");
+    }
 
-	// Destroy the double queue
-	// double_queue_destroy(&p_double_queue);
+    // #9 - map
+    {
 
-	// success
-	return EXIT_SUCCESS;
+        // map to upper case
+        double_queue_map(p_double_queue, string_upper_case, NULL);
+
+        // checkpoint
+        checkpoint(p_double_queue, "after map upper case");
+    }
+
+    // #10 - destroy 
+    {
+
+        // destroy the double ended queue
+        double_queue_destroy(&p_double_queue, default_allocator);
+
+        // checkpoint
+        checkpoint(p_double_queue, "after destroy");
+    }
+    
+    
+    // #11 - from binary
+    {
+
+        // initialized data
+        char buf[1024] = { 0 };
+        
+        // read a buffer from a file
+        p_f = fopen("resources/reflection/double_queue.bin", "rb"),
+        fread(buf, file_len, 1, p_f),
+        fclose(p_f);
+
+        // reflect a double ended queue from the buffer
+        double_queue_unpack(&p_double_queue, buf, string_unpack),
+
+        // checkpoint
+        checkpoint(p_double_queue, "after parse");
+    }
+
+    // #12 - hash 2
+    {
+
+        // initialized data
+        h2 = double_queue_hash(p_double_queue, (fn_hash64 *)string_hash);
+
+        // print the hash
+        printf("hash 2 -> 0x%llx\n", h2);
+
+        // error check
+        if ( h1 != h2 ) log_error("Error: hash 1 != hash 2\n");
+
+        // checkpoint
+        checkpoint(p_double_queue, "after hash 2");
+    }
+
+    // #13 - destroy
+    {
+
+        // destroy the cache
+        double_queue_destroy(&p_double_queue, default_allocator);
+
+        // checkpoint
+        checkpoint(p_double_queue, "after destroy");
+    }
+
+    // #14 - end
+    checkpoint(p_double_queue, "end");
+   
+    // success
+    return EXIT_SUCCESS;
+}
+
+int checkpoint ( double_queue *p_double_queue, const char *p_event )
+{
+    
+    // static data
+    static int step = 0;
+     
+    // print the cache
+    if ( NULL == p_double_queue )
+		log_info("#%d - Double queue %s: ", step, p_event),
+		printf("NULL\n");
+    else
+		log_info("#%d - Double queue %s:\n", step, p_event),
+		double_queue_fori(p_double_queue, string_print),
+		putchar('\n');
+    
+    // increment counter
+    step++;
+    
+    // success
+    return 1;
+}
+    
+void *string_upper_case ( void *p_value )
+{
+
+    // initialized data
+    char *p_string = strdup((char *)p_value),
+         *p_result = p_string;
+         
+    // iterate upto null terminator
+    while (*p_string)
+    {
+
+        // initialized data
+        char c = *p_string;
+
+        // convert offending characters
+        if ( islower(c) ) c = toupper(c);
+        
+        // store the upper case character
+        *p_string++ = c;
+    }
+
+    // success
+    return p_result;
+}
+
+void *string_lower_case ( void *p_value )
+{
+
+    // initialized data
+    char *p_string = strdup((char *)p_value),
+         *p_result = p_string;
+         
+    // iterate upto null terminator
+    while (*p_string)
+    {
+
+        // initialized data
+        char c = *p_string;
+
+        // convert offending characters
+        if ( isupper(c) ) c = tolower(c);
+        
+        // store the lower case character
+        *p_string++ = c;
+    }
+
+    // success
+    return p_result;
+}
+
+void string_print ( void *p_value, int i )
+{
+    
+    // print the element
+    printf("%d -> %s\n", i, (char *)p_value);
+    
+    // done
+    return ;
+}
+
+int string_compare ( const void *const p_a, const void *const p_b )
+{
+    char *a = *(char **)p_a,
+         *b = *(char **)p_b;
+
+    return strcmp(a, b);
+}
+
+int string_pack ( void *p_buffer, const void *const p_value )
+{
+
+    // done
+    return pack_pack(p_buffer, "%s", p_value);
+}
+
+int string_unpack ( void *const p_value, void *p_buffer )
+{
+
+    // initialized data
+    char       **pp_value        = (char **) p_value;
+    int          result          = 0;
+    char        *p_string        = NULL;
+    const char   _string  [1024] = { 0 };
+
+    // unpack the buffer
+    result = pack_unpack(p_buffer, "%s", &_string);
+
+    // duplicate the string
+    p_string = strdup(_string);
+
+    // return a pointer to the caller
+    *pp_value = p_string;
+
+    // done
+    return result;
+}
+
+hash64 string_hash ( const void *const string, unsigned long long unused )
+{
+
+    // unused
+    (void)unused;
+
+    // done
+    return hash_crc64(string, strlen(string));
 }
